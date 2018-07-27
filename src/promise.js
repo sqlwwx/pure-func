97271f6ae0fe96a8a9bf07808e2c1d3f60268f37
sqlwwx <wwx_2012@hotmail.com>
2018-07-27 17:41:38 +0800

    update

--- src/promise.js
+++ src/promise.js
@@ -1,14 +1,12 @@
 const { random } = require('./number')
 
-function noop () {}
-
 export const sleep = (timeout = 1000) => new Promise(resolve => {
   setTimeout(resolve, timeout)
 })
 
-export const doWhile = async (action, condition, cb = noop) => action().then(data => {
-  cb(data)
-  return condition(data) ? doWhile(action, condition, cb) : Promise.resolve(data)
+export const doWhile = async (action, condition, doItem) => action().then(data => {
+  doItem && doItem(data)
+  return condition(data) ? doWhile(action, condition, doItem) : Promise.resolve(data)
 })
 
 export const sleepRandom = (from, to) => sleep(random(from, to))
