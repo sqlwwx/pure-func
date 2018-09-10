a2df23785f9df23ed3cdaea4d46f253cd29d8043
sqlwwx <wwx_2012@hotmail.com>
2018-09-10 09:49:43 +0800

    update random

--- src/number.js
+++ src/number.js
@@ -1,2 +1,2 @@
-export const random = (from, to) => from + Math.floor(Math.random() * (to - from))
+export const random = (from, to) => from + Math.floor(Math.random() * (to + 1 - from) - 0.0000001)
 export const randomNumberStr = (length = 16) => Math.random().toString().slice(2, length + 2)
--- src/number.test.js
+++ src/number.test.js
@@ -3,10 +3,19 @@ const { random, randomNumberStr } = require('./number')
 /* eslint-env jest */
 describe('number', () => {
   it('random', () => {
+    let counts = {}
+    for (var i = 0, len = 10000; i < len; i++) {
+      const num = random(0, 1)
+      counts['count_' + num] = counts['count_' + num] || 0
+      counts['count_' + num]++
+    }
+    expect(
+      Math.abs(counts['count_0'] - counts['count_1'])
+    ).toBeLessThan(200)
     for (var i = 0, len = 1000; i < len; i++) {
       let num = random(10, 20)
       expect(num).toBeGreaterThanOrEqual(10)
-      expect(num).toBeLessThan(20)
+      expect(num).toBeLessThanOrEqual(20)
     }
   })
   it('randomNumberStr', () => {
