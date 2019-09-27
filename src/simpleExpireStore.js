5652c87c7845507ba032e3da32bcd74aab3398d4
sqlwwx <wwx@wuweixing.com>
2019-09-27 18:03:07 +0800

    Update simpleExpireStore.js

--- src/simpleExpireStore.js
+++ src/simpleExpireStore.js
@@ -12,11 +12,12 @@ const simpleExpireStore = (obj = {}, timeout = 1000, checkInterval = 60000) => {
   return new Proxy(obj, {
     get (target, name) {
       const value = target[name]
-      if (!value) {
-        return undefined
-      }
+      // no cache for function
       if (value instanceof Function) {
         return value
+      }
+      if (!value) {
+        return undefined
       }
       if (value.expiredAt < Date.now()) {
         Reflect.deleteProperty(target, name)
