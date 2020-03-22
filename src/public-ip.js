4c1c8338b43284bd16278dd9ebf740d3c525e760
sqlwwx <wwx@wuweixing.com>
2020-03-22 15:16:37 +0800

    chore: update

--- src/public-ip.js
+++ src/public-ip.js
@@ -1,12 +1,33 @@
 const got = require('got')
+const assert = require('assert')
+
+const isIp = /\d+\.\d+\.\d+\.\d+/
 
 // https://raw.githubusercontent.com/rsp/scripts/master/externalip-benchmark
-const defaultIpServer = 'http://whatismyip.akamai.com/'
+const defaultIpServers = [
+  'http://whatismyip.akamai.com/',
+  'http://ipecho.net/plain',
+  'http://icanhazip.com/'
+]
 let v4ip
 
-export const v4 = async ({ reload, url } = {}) => {
+const loadData = (urls, index = 0) => {
+  if (urls[index]) {
+    return got(urls[index], {
+      timeout: 1500,
+      retryCount: 0,
+      resolveBodyOnly: true
+    }).then(ip => {
+      assert.match(ip, isIp)
+      return ip
+    }).catch(() => loadData(urls, index + 1))
+  }
+  return Promise.reject(new Error('LOAD_PUBLIC_IP_FAILED'))
+}
+
+export const v4 = async ({ reload, urls } = {}) => {
   if (!v4ip || reload) {
-    v4ip = await got(url || defaultIpServer, { resolveBodyOnly: true })
+    v4ip = loadData(urls || defaultIpServers)
   }
   return v4ip
 }
--- src/public-ip.test.js
+++ src/public-ip.test.js
@@ -2,6 +2,8 @@ const { v4 } = require('./public-ip')
 
 const isIp = /\d+\.\d+\.\d+\.\d+/
 
+jest.setTimeout(1000 * 60 * 5)
+
 /* eslint-env jest */
 describe('public-ip', () => {
   it('v4', async () => {
@@ -11,18 +13,45 @@ describe('public-ip', () => {
     now = Date.now()
     expect(isIp.test(await v4())).toEqual(true)
     expect(Date.now() - now).toBeLessThan(2)
+    expect(v4({
+      reload: true,
+      urls: [
+        'http://notExist.com/'
+      ]
+    })).rejects.toHaveProperty('message', 'LOAD_PUBLIC_IP_FAILED')
   })
   it('v4 reload', async () => {
     const now = Date.now()
     expect(isIp.test(await v4({ reload: true }))).toEqual(true)
     expect(Date.now() - now).toBeGreaterThanOrEqual(2)
   })
-  it('v4 customer url', async () => {
+  it('v4 customer urls', async () => {
     const now = Date.now()
     expect(isIp.test(await v4({
       reload: true,
-      url: 'http://icanhazip.com/'
+      urls: ['http://icanhazip.com/']
     }))).toEqual(true)
     expect(Date.now() - now).toBeGreaterThanOrEqual(2)
+  })
+  it('v4 retry', async () => {
+    const now = Date.now()
+    const ipv4 = await v4({
+      reload: true,
+      urls: [
+        'http://notExist.com/',
+        'http://ipecho.net/plain',
+        'http://whatismyip.akamai.com/'
+      ]
+    })
+    expect(isIp.test(ipv4)).toEqual(true)
+    expect(Date.now() - now).toBeGreaterThanOrEqual(2)
+  })
+  it('v4 error', async () => {
+    expect(v4({
+      reload: true,
+      urls: [
+        'http://notExist.com/'
+      ]
+    })).rejects.toHaveProperty('message', 'LOAD_PUBLIC_IP_FAILED')
   })
 })
